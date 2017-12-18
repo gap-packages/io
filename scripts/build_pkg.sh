e32974644a37e86fbb84d1d2611cf8b911fbde11
Max Horn <max@quendi.de>
2017-12-18 15:48:35 +0100

    Overhaul travis integration

--- scripts/build_pkg.sh
+++ scripts/build_pkg.sh
@@ -5,25 +5,20 @@ set -ex
 export CFLAGS="$CFLAGS -fprofile-arcs -ftest-coverage"
 export LDFLAGS="$LDFLAGS -fprofile-arcs"
 
-if [[ $ABI = 32 ]]
-then
+if [[ $ABI = 32 ]]; then
     export CFLAGS="$CFLAGS -m32"
     export LDFLAGS="$LDFLAGS -m32"
 fi
 
-if [[ $HPCGAP = yes ]]
-then
-  # HACK until GAP and package build systems are improved:
-  # Add flags so that Boehm GC and libatomic headers are found
-  CPPFLAGS="-I$GAPROOT/extern/install/gc/include -I$GAPROOT/extern/install/libatomic_ops/include $CPPFLAGS"
-  export CPPFLAGS
-fi
-
-
 # build this package
-if [[ -x autogen.sh ]]
-then
+if [[ -x autogen.sh ]]; then
     ./autogen.sh
     ./configure --with-gaproot=$GAPROOT
     make -j4 V=1
+elif [[ -x configure ]]; then
+    ./configure $GAPROOT
+    make -j4
 fi
+
+# trick to allow the package directory to be used as a GAP root dir
+ln -s . pkg
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-GAP="$GAPROOT/bin/gap.sh --quitonbreak"
+GAP="$GAPROOT/bin/gap.sh -l $PWD; --quitonbreak"
 
-mkdir $COVDIR
-$GAP --cover $COVDIR/test.coverage tst/testall.g
+# unless explicitly turned off, we collect coverage data
+if [[ -z $NO_COVERAGE ]]; then
+    mkdir $COVDIR
+    GAP="$GAP --cover $COVDIR/test.coverage"
+fi
+
+$GAP tst/testall.g
